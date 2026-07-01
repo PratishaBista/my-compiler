4416eda5f0482b56b4dff8dc1cd805284f5dfa13
PratishaBista <bista.pratisha@gmail.com>
2026-07-01 22:01:20 +0545

    A basic expression evaluator (only demo)

---
+++ Minsk/Minsk/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace mc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(line))
+                    return;
+
+                if (line == "1 + 2 * 3")
+                    Console.WriteLine("7");
+                else
+                    Console.WriteLine("I don't know how to answer that.");
+            }
+        }
+    }
+}
